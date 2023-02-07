84a4d09c331fa290272e8807f3e1e6384785b01e
KitiDev <kitiexecutiveoffices@gmail.com>
2023-02-07 10:07:43 +0000

    my light switch is dysfunctional but here my progress

--- Game project/KITI enigma jam/Assets/Scripts/LightSwitch/lightOnOff.cs
+++ Game project/KITI enigma jam/Assets/Scripts/LightSwitch/lightOnOff.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,34 +7,47 @@ public class lightOnOff : MonoBehaviour
 {
     public GameObject txtToDisplay;
 
+    public GameObject player;
+
     private bool playerInZone;
 
     public GameObject lightOrObject;
+
+    public bool lightIsON;
 
     // Start is called before the first frame update
     void Start()
     {
         playerInZone = false;
         txtToDisplay.SetActive(false);
+        Debug.Log("Your text");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerInZone && Input.GetKeyDown(KeyCode.F))
+        Console.WriteLine("Script is updating");
+        if (playerInZone && Input.GetKeyDown(KeyCode.F) && lightIsON == false)
         {
             lightOrObject.SetActive(!lightOrObject.activeSelf);
             gameObject.GetComponent<AudioSource>().Play();
-            gameObject.GetComponent<Animator>().Play("switch");
+            gameObject.GetComponent<Animator>().Play("LightSwitchAnimation01");
+        }
+        if (playerInZone && Input.GetKeyDown(KeyCode.F) && lightIsON == true)
+        {
+            lightOrObject.SetActive(!lightOrObject.activeSelf);
+            gameObject.GetComponent<AudioSource>().Play();
+            gameObject.GetComponent<Animator>().Play("LightSwitchAnimation02");
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "player")
+        if (other.gameObject.tag == "Player")
         {
             txtToDisplay.SetActive(true);
             playerInZone = true;
+            Console.WriteLine("player is inside colloder");
         }
     }
 
