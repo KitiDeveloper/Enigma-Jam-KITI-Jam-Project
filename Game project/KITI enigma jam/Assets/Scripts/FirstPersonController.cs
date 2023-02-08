69647d8d9eac1c10a2ef7258b6ed72fd4f87d55b
KitiDev <kitiexecutiveoffices@gmail.com>
2023-02-08 09:13:01 +0000

    I added the ability to jump for the player

--- Game project/KITI enigma jam/Assets/Scripts/FirstPersonController.cs
+++ Game project/KITI enigma jam/Assets/Scripts/FirstPersonController.cs
@@ -7,24 +7,30 @@ using UnityEngine;
 public class FirstPersonController : MonoBehaviour
 {
     public bool canMove { get; private set; } = true;
-    private bool isRunning => canSprint && Input.GetKey(RunKey);
+    private bool isRunning => canRun && Input.GetKey(RunKey);
+    private bool shouldJump => Input.GetKeyDown(JumpKey) && characterController.isGrounded;
 
     [Header("Functional Bools")]
-    [SerializeField] private bool canSprint = true;
+    [SerializeField] private bool canRun = true;
+    [SerializeField] private bool canJump = true;
 
     [Header("Controls")]
     [SerializeField] private KeyCode RunKey = KeyCode.LeftShift;
+    [SerializeField] private KeyCode JumpKey = KeyCode.Space;
 
     [Header("Movement Parameter")]
     [SerializeField] private float walkSpeed = 3.0f;
     [SerializeField] private float runSpeed = 6.0f;
-    [SerializeField] private float gravity = 30.0f;
 
     [Header("Look Parameter")]
     [SerializeField, Range(0.01f, 10)] private float lookSpeedX = 2.0f;
     [SerializeField, Range(0.01f, 10)] private float lookSpeedY = 2.0f;
-    [SerializeField, Range(0.01f, 120)] private float upLookLimit = 2.0f;
-    [SerializeField, Range(0.01f, 120)] private float downLookLimit = 2.0f;
+    [SerializeField, Range(0.01f, 2)] private float upLookLimit = 2.0f;
+    [SerializeField, Range(0.01f, 2)] private float downLookLimit = 2.0f;
+
+    [Header("Look Parameter")]
+    [SerializeField] private float jumpForce = 8.0f;
+    [SerializeField] private float gravity = 30.0f;
 
     private Camera playerCamera;
     private CharacterController characterController;
@@ -52,6 +58,11 @@ public class FirstPersonController : MonoBehaviour
             HandleMovementInput();
             HandleMouseLook();
 
+            if(canJump)
+            {
+                HandleJump();
+            }
+
             ApplyFinalMovements();
         }
     }
@@ -73,6 +84,14 @@ public class FirstPersonController : MonoBehaviour
         rotationX = Mathf.Clamp(rotationX, -upLookLimit, downLookLimit);
         playerCamera.transform.localRotation = quaternion.Euler(rotationX, 0, 0);
         transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeedX, 0);
+    }
+
+    private void HandleJump()
+    {
+        if(shouldJump)
+        {
+            moveDirection.y = jumpForce;
+        }
     }
 
     private void ApplyFinalMovements()
