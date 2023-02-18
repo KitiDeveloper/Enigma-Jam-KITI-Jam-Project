67c5e614ab25143b38e3f42ec962c9cac79dc369
Barribob Development <barribobdevelopment@gmail.com>
2023-02-18 10:30:46 -0700

    Add basic main menu

---
+++ Game project/KITI enigma jam/Assets/Scripts/UI/QuitButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class QuitButton : MonoBehaviour
+    {
+        [SerializeField] private Button quitButton;
+
+        private void Start()
+        {
+            quitButton.onClick.AddListener(Quit);
+        }
+        
+        private void Quit()
+        {
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+            Application.Quit();
+        }
+    }
+}
---
+++ Game project/KITI enigma jam/Assets/Scripts/UI/StartGameButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class StartGameButton : MonoBehaviour
+    {
+        [SerializeField] private GameObject loadingScreen;
+        [SerializeField] private Button button;
+        [SerializeField] private int gameSceneIndex;
+
+        private void Start()
+        {
+            loadingScreen.SetActive(false);
+            button.onClick.AddListener(StartGame);
+        }
+
+        private void StartGame()
+        {
+            loadingScreen.SetActive(true);
+            SceneManager.LoadSceneAsync(gameSceneIndex);
+        }
+    }
+}
