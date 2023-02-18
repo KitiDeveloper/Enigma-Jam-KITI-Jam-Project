e9ff94741e8ed875a307a63f53671e1cc45fe3f7
Barribob Development <barribobdevelopment@gmail.com>
2023-02-18 13:30:05 -0700

    Add ammo component to living room puzzle

---
+++ Game project/KITI enigma jam/Assets/Scripts/GameObjects/Puzzle/PuzzleProgressInteractable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace GameObjects.Puzzle
+{
+    public class PuzzleProgressInteractable : Interactable
+    {
+        [SerializeField] private ClockPuzzle clockPuzzle;
+        [SerializeField] private int progressState;
+        public override void OnInteract()
+        {
+            clockPuzzle.SetPuzzleProgress(progressState);
+            gameObject.SetActive(false);
+        }
+
+        public override void OnFocus()
+        {
+        }
+
+        public override void OnLoseFocus()
+        {
+        }
+    }
+}
