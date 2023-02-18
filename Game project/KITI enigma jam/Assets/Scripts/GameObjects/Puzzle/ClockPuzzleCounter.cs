e94bd7832393186de56800e63083b3c9cc933314
Barribob Development <barribobdevelopment@gmail.com>
2023-02-18 09:52:02 -0700

    Add win sequence sound

--- Game project/KITI enigma jam/Assets/Scripts/GameObjects/Puzzle/ClockPuzzleCounter.cs
+++ Game project/KITI enigma jam/Assets/Scripts/GameObjects/Puzzle/ClockPuzzleCounter.cs
@@ -6,12 +6,18 @@ namespace GameObjects.Puzzle
     public class ClockPuzzleCounter : MonoBehaviour
     {
         private int numPuzzlesSolved;
+        [SerializeField] private int puzzlesToSolveToWin = 3;
         [SerializeField] private TextMeshProUGUI text;
+        [SerializeField] private WinSequence winSequence;
         
         public void PuzzleSolved()
         {
             numPuzzlesSolved++;
             text.text = $"Weapons discovered: {numPuzzlesSolved}";
+            if (numPuzzlesSolved == puzzlesToSolveToWin)
+            {
+                winSequence.Play();
+            }
         }
     }
 }
