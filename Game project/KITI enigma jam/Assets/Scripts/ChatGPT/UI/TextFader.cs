bdeacaf558b96db93ff436a494161e4b221e94e7
Barribob Development <barribobdevelopment@gmail.com>
2023-02-18 20:34:20 -0700

    Add fade win sequence; update control image

---
+++ Game project/KITI enigma jam/Assets/Scripts/ChatGPT/UI/TextFader.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace ChatGPT.UI
+{
+    public class TextFader : MonoBehaviour
+    {
+        public TextMeshProUGUI textComponent;
+        public float fadeTime = 1f;
+        public float waitTime = 1f;
+        public float initialWaitTime = 3f;
+
+        private Color originalColor;
+
+        void Start()
+        {
+            originalColor = textComponent.color;
+            textComponent.color = new Color(originalColor.r, originalColor.g, originalColor.b, 0f);
+        }
+
+        public void StartFade()
+        {
+            StartCoroutine(Fade());
+        }
+
+        IEnumerator Fade()
+        {
+            yield return new WaitForSeconds(initialWaitTime);
+            
+            float t = 0f;
+
+            // Fade in
+            while (t < fadeTime)
+            {
+                t += Time.deltaTime;
+                float alpha = Mathf.Lerp(0f, 1f, t / fadeTime);
+                textComponent.color = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(waitTime);
+
+            t = 0f;
+
+            // Fade out
+            while (t < fadeTime)
+            {
+                t += Time.deltaTime;
+                float alpha = Mathf.Lerp(1f, 0f, t / fadeTime);
+                textComponent.color = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
+                yield return null;
+            }
+
+            // textComponent.color = originalColor;
+        }
+    }
+}
--- Game project/KITI enigma jam/Assets/Scripts/GameObjects/WinSequence.cs
+++ Game project/KITI enigma jam/Assets/Scripts/GameObjects/WinSequence.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using System.Collections;
+using ChatGPT.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 namespace GameObjects
 {
@@ -8,14 +11,48 @@ namespace GameObjects
         [SerializeField] private AudioSource audioSource;
         [SerializeField] private AudioClip audioClip;
         [SerializeField] private GameObject[] particleSystemsToEnable;
+        [FormerlySerializedAs("trackLength")] [SerializeField] private int delayLength;
+        private bool startFade;
+        private float fadeTime;
+        private float initialVolume;
+        [SerializeField] private CanvasGroup fadeCanvas;
+        [SerializeField] private int mainSceneIndex;
+        [SerializeField] private TextFader winText;
 
         public void Play()
         {
+            initialVolume = audioSource.volume;
             audioSource.Stop();
             audioSource.PlayOneShot(audioClip);
             foreach (var o in particleSystemsToEnable)
             {
                 o.SetActive(true);
+            }
+            winText.StartFade();
+
+            StartCoroutine(nameof(StopMusicAndFade));
+        }
+
+        IEnumerator StopMusicAndFade()
+        {
+            yield return new WaitForSeconds(delayLength);
+            startFade = true;
+        }
+
+        private void Update()
+        {
+            if (startFade)
+            {
+                fadeTime += Mathf.Min(1f, Time.deltaTime);
+                var time = fadeTime * 0.5f;
+                fadeCanvas.alpha = Mathf.Lerp(0.0f, 1.0f, time);
+                audioSource.volume = Mathf.Lerp(initialVolume, 0.0f, time);
+                if (time > 1)
+                {
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+                    SceneManager.LoadScene(mainSceneIndex);
+                }
             }
         }
     }
